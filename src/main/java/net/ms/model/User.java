100ae10435873e48837d901d79ad6c5d1bff7319
Ignition-Code <manduviservicios@gmail.com>
2024-06-19 20:15:24 -0400

    User
    * Add user model

--- src/main/java/net/ms/model/User.java
+++ src/main/java/net/ms/model/User.java
@@ -1,2 +1,46 @@
-package net.ms.model;public class User {
+package net.ms.model;
+
+public class User {
+    private String userName;
+    private String userPassword;
+    private boolean userAdmin;
+
+    public User(String userName, String userPassword, boolean userAdmin) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userAdmin = userAdmin;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public boolean isUserAdmin() {
+        return userAdmin;
+    }
+
+    public void setUserAdmin(boolean userAdmin) {
+        this.userAdmin = userAdmin;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario {" +
+                "nombre='" + userName + '\'' +
+                ", contraseña='" + userPassword + '\'' +
+                ", admin=" + userAdmin +
+                '}';
+    }
 }
